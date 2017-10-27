c58550182e25a500d0319a10a406e6292b6e31a7
Unknown <minhtri4422@gmail.com>
2017-10-27 22:37:17 +1100

    signup linked to userdetail database

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {ItemPage} from '../item/item';
+import{LoginPage} from '../login/login';
 import {User} from '../../models/user';
 import {AngularFireAuth} from "angularfire2/auth";
+import * as firebase from 'firebase/app'; // for typings
+import { FirebaseApp } from 'angularfire2'; // for methods
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database-deprecated';
 /**
  * Generated class for the SignupPage page.
  *
@@ -18,7 +24,9 @@ import {AngularFireAuth} from "angularfire2/auth";
 })
 export class SignupPage {
 user = {}as User;
-  constructor(private afAuth:AngularFireAuth,
+  constructor(private afAuth:AngularFireAuth,public http: Http, 
+    public db: AngularFireDatabase,
+    public fbe: FirebaseApp,
     public navCtrl: NavController, public navParams: NavParams) {
   }
 
@@ -29,10 +37,17 @@ user = {}as User;
     try{
     const result =this.afAuth.auth.createUserWithEmailAndPassword(user.email,user.password);
     console.log(result);
+    this.navCtrl.setRoot(LoginPage);
     }
     catch(e){
 
       console.error(e);
-      }}    }
+      }}   
+    
+      addUser(user:User){
+        this.db.list('/users/').push({FirstName: user.firstname, LastName: user.lastname, Email:user.email, Phone:user.phoneno,StudentID:user.studentid,Address:user.address})}
+        
+    
 
-
+      
+        }
