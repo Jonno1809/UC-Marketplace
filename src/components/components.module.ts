a3c686929710b6465cb2306fdc57bdc6fd3e332e
Unknown <immaturestephen@gmail.com>
2017-11-01 15:47:58 +1100

    deleted component module

--- src/components/components.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { ItemdetailsComponent } from './itemdetails/itemdetails';
-@NgModule({
-	declarations: [ItemdetailsComponent],
-	imports: [],
-	exports: [ItemdetailsComponent]
-})
-export class ComponentsModule {}
